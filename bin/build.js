ff86ca01bc6a3eff762eff3b5b06155efd296bf7
Mohammed Ali <mafnannah@gmail.com>
2024-09-23 18:50:15 +0300

    update

--- bin/build.js
+++ bin/build.js
@@ -46,5 +46,5 @@ const defaultOptions = {
 compile({
     ...defaultOptions,
     entryPoints: ['./resources/js/index.js'],
-    outfile: './resources/dist/pdf-arabic.js',
+    // outfile: './resources/dist/pdf-arabic.js',
 })
--- src/PdfArabic.php
+++ src/PdfArabic.php
@@ -1,5 +1,51 @@
 <?php
 
 namespace Mfnannah\PdfArabic;
+use Arphp\Glyphs;
+use Closure;
+use Dompdf\Dompdf;
+use Filament\Actions\Concerns\CanCustomizeProcess;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+class PdfArabic extends Action{
+    use CanCustomizeProcess;
 
-class PdfArabic {}
+    protected ?Closure $mutateRecordDataUsing = null;
+
+    public static function getDefaultName(): ?string
+    {
+        return 'arabicPdf';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label('PDF');
+
+        $this->modalHeading(fn (): string => 'PDF');
+
+        $this->successNotificationTitle(__('filament-actions::edit.single.notifications.saved.title'));
+
+        $this->icon('heroicon-o-arrow-down');
+
+        $this->action(function (): void {
+            $this->process(function (array $data, Model $record, Table $table) {
+                $arabic = new Glyphs;
+                $data = [
+                    'record' => $record,
+                    'arabic' => $arabic,
+                ];
+                $html = view('templates.pdf', $data)->render();
+                $dompdf = new Dompdf;
+                $dompdf->loadHtml($html);
+                $dompdf->setPaper('A4', 'portrait');
+                $dompdf->render();
+                $dompdf->stream('document.pdf');
+            });
+
+            $this->success();
+        });
+    }
+}
